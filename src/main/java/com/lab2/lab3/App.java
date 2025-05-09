5732d5e1c49b9967fa368b076febfd46dfe2afa8
Manohari17 <manoharisannakki@gmai.com>
2025-05-09 12:30:07 +0530

    first commit

---
+++ src/main/java/com/lab2/lab3/App.java
@@ -0,0 +1,13 @@
+package com.lab2.lab3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
